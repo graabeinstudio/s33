3721c1bbbdbad74b2e234f494b2d5f8a467612c5
Kari Eline Strandjord <kari.eline.strandjord@schibsted.com>
2020-12-16 22:20:03 +0100

    fix: etikketter

--- web/src/components/batch-preview.js
+++ web/src/components/batch-preview.js
@@ -1,12 +1,12 @@
-import { Link } from "gatsby";
-import React from "react";
-import { cn, buildImageObj } from "../lib/helpers";
-import { imageUrlFor } from "../lib/image-url";
+import {Link} from 'gatsby'
+import React from 'react'
+import {cn, buildImageObj} from '../lib/helpers'
+import {imageUrlFor} from '../lib/image-url'
 
-import styles from "./batch-preview.module.css";
-import { responsiveTitle3 } from "./typography.module.css";
+import styles from './batch-preview.module.css'
+import {responsiveTitle3} from './typography.module.css'
 
-function BatchPreview(props) {
+function BatchPreview (props) {
   return (
     <Link className={styles.root} to={`/batch/${props.slug.current}`}>
       <div className={styles.leadMediaThumb}>
@@ -14,7 +14,6 @@ function BatchPreview(props) {
           <img
             src={imageUrlFor(buildImageObj(props.label))
               .width(600)
-              .height(Math.floor((9 / 16) * 600))
               .url()}
             alt={props.label.alt}
           />
@@ -22,7 +21,7 @@ function BatchPreview(props) {
       </div>
       <h3 className={cn(responsiveTitle3, styles.name)}>{props.name}</h3>
     </Link>
-  );
+  )
 }
 
-export default BatchPreview;
+export default BatchPreview
